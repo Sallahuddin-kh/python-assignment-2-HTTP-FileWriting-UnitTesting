6323148e9964beaf3ed49099b58090cbfea12b9b
coeus <sallahuddinkh@gmail.com>
2021-10-13 15:39:06 +0500

    Refining test cases

--- testcases.py
+++ testcases.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 import request
 
@@ -9,10 +10,10 @@ def test_write_to_file1(param):
 def test_cwrite_to_file2(param):
     assert request.write_to_file(param) == False
 
-@pytest.mark.parametrize("param",['https://api.ekomi.de/v3/getFeedback?auth=100809|3222cc91604a81845f8c3c0d7&type=json&range=6m'])
-def test_fetch_from_HTTP_to_File(param):
-    assert type(request.fetch_from_HTTP_to_File(param)) == list
+@pytest.mark.parametrize("param",["https://api.ekomi.de/v3/getFeedback?auth=100809|3222cc91604a81845f8c3c0d7&type=json&range=6m"])
+def test_fetch_from_HTTP_to_File1(param):
+    assert request.fetch_from_HTTP_to_File(param) is None
 
-@pytest.mark.parametrize("param",['somemememe.msms'])
-def test_fetch_from_HTTP_to_File(param):
-    assert True == False
+@pytest.mark.parametrize("param",['somemememe.msms?'])
+def test_fetch_from_HTTP_to_File2(param):
+    assert type(request.fetch_from_HTTP_to_File(param)) == dict
