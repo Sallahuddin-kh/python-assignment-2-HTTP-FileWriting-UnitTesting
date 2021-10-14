311817dd1e7449bce7880137e988e688af392809
coeus <sallahuddinkh@gmail.com>
2021-10-14 14:13:44 +0500

    Refining test exception handling

--- request.py
+++ request.py
@@ -11,9 +11,9 @@ def write_to_file(data)->bool:
                 f.close()
             return True
         else:
-            raise ValueError
-    except ValueError as e:
-        print("Input Datatype should be list or Dict")
+            return False
+    except OSError as e:
+        print("A exception occured during file writing: ",e)
         return False
 
 def fetch_from_HTTP_to_File(url:str = "https://api.ekomi.de/v3/getFeedback?auth=100809|3222cc91604a81845f8c3c0d7&type=json&range=6m"):
@@ -21,10 +21,18 @@ def fetch_from_HTTP_to_File(url:str = "https://api.ekomi.de/v3/getFeedback?auth=
         r = requests.get(url)
         r.raise_for_status()
         return_list = r.json()
+        if(r.status_code == 204):
+            return None
         if not write_to_file(return_list):
             raise ValueError
-        return None
-    except (ConnectionError,ValueError,HTTPError,Timeout,TooManyRedirects) as c:
-        print("ok")
+        return return_list
+    except (ConnectionError,HTTPError,Timeout,TooManyRedirects) as c:
+        print("HTTP exception occurred: ",c)
         error_data = '{"response": "error", "message": "An exception occured"}'
         return json.loads(error_data)
+    except ValueError:
+        print("Unable to write data to the file. Incorrect format")
+        error_data = '{"response": "error", "message": "An exception occured"}'
+        return json.loads(error_data)
+
+print(type(fetch_from_HTTP_to_File()))
