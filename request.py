eade083e42d4907a3ffafae271010aaef24a5dc7
coeus <sallahuddinkh@gmail.com>
2021-10-15 11:06:15 +0500

    Adding some more test cases and comments

--- request.py
+++ request.py
@@ -3,10 +3,16 @@ from requests.exceptions import *
 import json
 
 def write_to_file(data)->bool:
+    """
+    Function Takes the data as dict or list and
+    writes the data to a file. Raises exception in case
+    of filing error. Does not allow to write in case
+    data is not in dict or list.
+    """
     try:
         if(type(data) is dict or type(data) is list):
             writer = json.dumps(data,ensure_ascii=False)
-            with open("demofile2.txt", "w") as f:
+            with open("file.txt", "w") as f:
                 f.write(writer)
                 f.close()
             return True
@@ -17,6 +23,10 @@ def write_to_file(data)->bool:
         return False
 
 def fetch_from_HTTP_to_File(url:str = "https://api.ekomi.de/v3/getFeedback?auth=100809|3222cc91604a81845f8c3c0d7&type=json&range=6m"):
+    """
+    Fetches data from url and calls the file writing function.
+    Raises exception if HTTP error or file writing error occurs.
+    """
     try:
         r = requests.get(url)
         r.raise_for_status()
@@ -35,4 +45,5 @@ def fetch_from_HTTP_to_File(url:str = "https://api.ekomi.de/v3/getFeedback?auth=
         error_data = '{"response": "error", "message": "An exception occured"}'
         return json.loads(error_data)
 
-print(type(fetch_from_HTTP_to_File()))
+a = fetch_from_HTTP_to_File(123)
+print(a["response"])
--- testcases.py
+++ testcases.py
@@ -11,9 +11,11 @@ def test_cwrite_to_file2(param):
 
 @pytest.mark.parametrize("param",["https://api.ekomi.de/v3/getFeedback?auth=100809|3222cc91604a81845f8c3c0d7&type=json&range=6m"])
 def test_fetch_from_HTTP_to_File1(param):
-    assert type(request.fetch_from_HTTP_to_File(param)) is list
+    assert type(request.fetch_from_HTTP_to_File(param)) == list
 
-@pytest.mark.parametrize("param",['http://somebadurl.example/'])
+@pytest.mark.parametrize("param",['http://somebadurl.example/',"Non url",123])
 def test_fetch_from_HTTP_to_File2(param):
-    assert type(request.fetch_from_HTTP_to_File(param)) is dict
-    
+    response_data = request.fetch_from_HTTP_to_File(param)
+    assert response_data['response'] == 'error'
+
+
